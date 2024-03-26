d0a6d99d36a409a68e64e0d89c7394f6f159cf73
Hanu-soni <pankajsoni93444@gmail.com>
2024-03-26 10:38:42 +0530

    updated_backend

---
+++ Models/Educator_info.js
@@ -0,0 +1,33 @@
+const mongoose=require('mongoose');
+
+// {firstName: 'PANKAJ', lastName: 'SONI', email: 'pankajsoni93444@gmai', password: '1234', businessName: 'Lets_learn', …}
+const Educator_info=new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    passwordHash: {
+        type: String,
+        required: true,
+    },
+    businessName: {
+        type: String,
+        required: true,
+    },
+    businessType: {
+        type: String,
+        required: true,
+    }
+   
+
+});
+
+exports.Educator_info = mongoose.model('Educator_info', Educator_info);
---
+++ Router/userRouter.js
@@ -0,0 +1,114 @@
+const { Educator_info } = require('../Models/Educator_info');
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const saltRounds = 10;
+
+
+// {firstName: 'PANKAJ', lastName: 'SONI', email: 'pankajsoni93444@gmai', password: '1234', businessName: 'Lets_learn', …}
+router.post('/login', async (req, res) => {
+    const user = await Educator_info.findOne({ email: req.body.email })
+    const secret = process.env.secret;
+    if (!user) {
+        return res.status(400).send({
+            success: false,
+            message: 'The user not found'
+        });
+    }
+
+    if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+        const token = jwt.sign(
+            {
+                userId: user.id
+            },
+            secret,
+            { expiresIn: '1d' }
+        )
+
+        res.status(200).send({
+            data: {
+                user: user.email,
+                token: token,
+            },
+            message: "Login successfull",
+            success: true
+        })
+    } else {
+        res.status(400).send();
+    }
+
+
+})
+
+
+
+//****when user hits register button, this api is called***
+//*REGISTER_API
+router.post('/register', async (req, res) => {
+
+    //getting the data and validating it.
+    let data = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        businessName: req.body.businessName,
+        businessType: req.body.businessType
+    }
+    //looping over the data so as to validate it.
+    //
+    for (const [key, value] of Object.entries(data)) {
+        console.log(key);
+        if (value.length === 0) {
+            return res.send({
+                success: false,
+                message: key + "  is missing"
+            })
+        }
+
+        var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (key === "email" && !emailRegex.test(data.email)) {
+            return res.send({
+                success: false,
+                message: key + "  is not valid"
+            })
+        }
+        if (key === "email") {
+            let query = await Educator_info.findOne({ email: data.email });
+            if (query) {
+                return res.send({
+                    success: false,
+                    message: key + "Already exist"
+                })
+
+            }
+
+        }
+
+        //if more data_validation is needed . we can add here
+    }
+    //console.log(data);
+
+    let user = new Educator_info(data)
+    user = await user.save();
+
+    // if (!user)
+    //     return res.status(400).send('the user cannot be created!')
+
+    res.status(200).send({
+        success: true,
+        message: "User added successfully",
+        data: {
+            name: user.firstName,
+            businessName: user.businessName
+        }
+    });
+})
+
+
+
+
+
+
+module.exports = router;
